26a06714cc7aec0c1168d6427085cc7a732a5ba6
halleluiaz <halleluiazeyohannes@gmail.com>
2022-12-21 22:21:48 -0500

    styling, add attr, tile count

--- js/script.js
+++ js/script.js
@@ -13,33 +13,33 @@ Purpose:
 
 // globals to be accessed
 var ScrabbleTiles = [] ;        // associative array adapted from Prof. Jesse Heines with letter ditribution
-ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9,  "image" : "../img/tiles/Scrabble_Tile_A.jpg"  } ;
-ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_B.jpg"  } ;
-ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_C.jpg"  } ;
-ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_D.jpg"  } ;
-ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12, "image" : "../img/tiles/Scrabble_Tile_E.jpg"} ;
-ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_F.jpg"  } ;
-ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3,  "image" : "../img/tiles/Scrabble_Tile_G.jpg"  } ;
-ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_H.jpg"  } ;
-ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9,  "image" : "../img/tiles/Scrabble_Tile_I.jpg"  } ;
-ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_J.jpg"  } ;
-ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_K.jpg"  } ;
-ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_L.jpg"  } ;
-ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_M.jpg"  } ;
-ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_N.jpg"  } ;
-ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8,  "image" : "../img/tiles/Scrabble_Tile_O.jpg"  } ;
-ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_P.jpg"  } ;
-ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_Q.jpg"  } ;
-ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_R.jpg"  } ;
-ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_S.jpg"  } ;
-ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_T.jpg"  } ;
-ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_U.jpg"  } ;
-ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_V.jpg"  } ;
-ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_W.jpg"  } ;
-ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_X.jpg"  } ;
-ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_Y.jpg"  } ;
-ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_Z.jpg"  } ;
-ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_Blank.jpg"  } ;
+ScrabbleTiles["A"] = { "value" : 1,  "original_distribution" : 9,  "number_remaining" : 9,  "image" : "../img/tiles/Scrabble_Tile_A.jpg"  } ;
+ScrabbleTiles["B"] = { "value" : 3,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_B.jpg"  } ;
+ScrabbleTiles["C"] = { "value" : 3,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_C.jpg"  } ;
+ScrabbleTiles["D"] = { "value" : 2,  "original_distribution" : 4,  "number_remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_D.jpg"  } ;
+ScrabbleTiles["E"] = { "value" : 1,  "original_distribution" : 12, "number_remaining" : 12, "image" : "../img/tiles/Scrabble_Tile_E.jpg"} ;
+ScrabbleTiles["F"] = { "value" : 4,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_F.jpg"  } ;
+ScrabbleTiles["G"] = { "value" : 2,  "original_distribution" : 3,  "number_remaining" : 3,  "image" : "../img/tiles/Scrabble_Tile_G.jpg"  } ;
+ScrabbleTiles["H"] = { "value" : 4,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_H.jpg"  } ;
+ScrabbleTiles["I"] = { "value" : 1,  "original_distribution" : 9,  "number_remaining" : 9,  "image" : "../img/tiles/Scrabble_Tile_I.jpg"  } ;
+ScrabbleTiles["J"] = { "value" : 8,  "original_distribution" : 1,  "number_remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_J.jpg"  } ;
+ScrabbleTiles["K"] = { "value" : 5,  "original_distribution" : 1,  "number_remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_K.jpg"  } ;
+ScrabbleTiles["L"] = { "value" : 1,  "original_distribution" : 4,  "number_remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_L.jpg"  } ;
+ScrabbleTiles["M"] = { "value" : 3,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_M.jpg"  } ;
+ScrabbleTiles["N"] = { "value" : 1,  "original_distribution" : 6,  "number_remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_N.jpg"  } ;
+ScrabbleTiles["O"] = { "value" : 1,  "original_distribution" : 8,  "number_remaining" : 8,  "image" : "../img/tiles/Scrabble_Tile_O.jpg"  } ;
+ScrabbleTiles["P"] = { "value" : 3,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_P.jpg"  } ;
+ScrabbleTiles["Q"] = { "value" : 10, "original_distribution" : 1,  "number_remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_Q.jpg"  } ;
+ScrabbleTiles["R"] = { "value" : 1,  "original_distribution" : 6,  "number_remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_R.jpg"  } ;
+ScrabbleTiles["S"] = { "value" : 1,  "original_distribution" : 4,  "number_remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_S.jpg"  } ;
+ScrabbleTiles["T"] = { "value" : 1,  "original_distribution" : 6,  "number_remaining" : 6,  "image" : "../img/tiles/Scrabble_Tile_T.jpg"  } ;
+ScrabbleTiles["U"] = { "value" : 1,  "original_distribution" : 4,  "number_remaining" : 4,  "image" : "../img/tiles/Scrabble_Tile_U.jpg"  } ;
+ScrabbleTiles["V"] = { "value" : 4,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_V.jpg"  } ;
+ScrabbleTiles["W"] = { "value" : 4,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_W.jpg"  } ;
+ScrabbleTiles["X"] = { "value" : 8,  "original_distribution" : 1,  "number_remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_X.jpg"  } ;
+ScrabbleTiles["Y"] = { "value" : 4,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_Y.jpg"  } ;
+ScrabbleTiles["Z"] = { "value" : 10, "original_distribution" : 1,  "number_remaining" : 1,  "image" : "../img/tiles/Scrabble_Tile_Z.jpg"  } ;
+ScrabbleTiles["_"] = { "value" : 0,  "original_distribution" : 2,  "number_remaining" : 2,  "image" : "../img/tiles/Scrabble_Tile_Blank.jpg"  } ;
 
 var stats = {                   // keep track of overall game score and how many tiles are left
     score: 0,
@@ -51,25 +51,40 @@ var current_word = {           // keep track of letters placed, the sum of their
     value: 0,
     valid: false,
     first_double: false,
-    second_double: false
+    second_double: false,
+    final_value: 0
 }
 
-var hand = [];
+// var hand = [];
 
+// random_tiles() generates 7 random tiles from the "bag" and makes them draggable. They can only be dropped
+// in an accepting droppable target otherwise they bounce back to where they were.
 function random_tiles() {
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+    var num = -1;
+    var letter = '';
+
     for(var i = 1; i <= 7; i++)
     {
-        var num = Math.floor(Math.random() * 27);
-        var letter = alphabet.charAt(num);
         var img_id = "letter" + i;
+        num = Math.floor(Math.random() * 27);       // random number from 0-26
+        letter = alphabet.charAt(num);
+        console.log("letter: " + letter + ", " + ScrabbleTiles[letter].number_remaining + " tiles left");
+        while(ScrabbleTiles[letter].number_remaining == 0){     // ensure that the tiles drawn from bag are available
+            num = Math.floor(Math.random() * 27);
+            letter = alphabet.charAt(num);
+        }
+        document.getElementById(img_id).setAttribute("value", letter);
         document.getElementById(img_id).src = ScrabbleTiles[letter].image;
+        ScrabbleTiles[letter].number_remaining--;
+        console.log(ScrabbleTiles[letter].number_remaining + " tiles left");
         $(img_id).draggable({
             revert: "invalid"
         });
     }
 }
 
+// droppable_slots() creates the droppable targets(board slots and rack) for the tiles
 function droppable_slots() {
     $(".droppable").droppable({
         accept: "img#draggable",
@@ -77,6 +92,11 @@ function droppable_slots() {
             // EDIT HERE
         }
     })
+}
+
+// updates the display above the board for the word based on what the user has placed on it
+function update_word() {
+
 }
 
 function submit_word(){
